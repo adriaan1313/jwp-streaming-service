9d4f2d2ef3907c13b7bd22c00a71f49923b12931
Bunnygamers <adriaan1313@outlook.com>
2023-07-14 17:59:51 +0200

    add a manifest
    because why not

--- home.html.js
+++ home.html.js
@@ -5,6 +5,7 @@ module.exports = (vars)=>`<!DOCTYPE HTML>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/regular.min.css">
 		<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
 		<link rel="stylesheet" href="/style/programme.css"/>
+		<link rel="manifest" href="/site.webmanifest">
 	</head>
 	</body>
 		${vars.menu}
